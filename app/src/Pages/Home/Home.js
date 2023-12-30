6f874ceaf75c6adcdd881e76b01df9639b84c98d
markrubin920 <markrubin920@gmail.com>
2023-12-29 21:18:47 -0500

    So close to done with homepage

--- app/src/Pages/Home/Home.js
+++ app/src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import "./Home.css";
 import Navigation from "../../Components/Navigation/Navigation";
 import Fiserv from "../../Media/Fiserv-Picture.jpg"
+import Workshop from "../../Media/Cloud-Workshop-Picture.jpg"
 import { NavLink } from "react-router-dom";
 
 const Home = () =>
@@ -22,16 +23,49 @@ const Home = () =>
             </section>
             <section className="exp-container">
                 <div className="experience-div-outer">
-                    <div className="exp-writing">
+                    <div className="writing">
                         <div className="exp-title"><h1>Work Experience</h1> </div>
                         <div className="exp-subtitle"><h1>BLA BLA BLA experience</h1> </div>
                         <div className="learn-more"><NavLink to = "/experience">Learn More</NavLink></div>
                     </div>
-                    <div className="experience-pic-div">
+                    <div className="pic-div">
                         <img src={Fiserv} alt = "Fiserv Internship Picture" className="exp-pic"/>
                     </div>
                 </div>
-                
+            </section>
+
+            <section className="ec-container">
+                <div className="ec-div-outer">
+                    <div className="pic-div">
+                        <img src={Workshop} alt = "Cloud Computing Workshop" className="exp-pic"/>
+                    </div>
+                    <div className="writing">
+                        <div className="ec-title"><h1>Extracurricular Activities</h1> </div>
+                        <div className="ec-subtitle"><h1>BLA BLA BLA Activities</h1> </div>
+                        <div className="learn-more"><NavLink to = "/extracurriculars">Learn More</NavLink></div>
+                    </div>
+                </div>
+            </section>
+            <section className="quote-section">
+                <p className="quote-text">
+                    BLA BLA BLA
+                </p>
+            </section>
+            <section className="contact-container">
+                <div className="get-in-touch">
+                    <h2 className="gt-text">Let's get in touch!</h2>
+                </div>
+                <div className="contact-info">
+                    <ul className="contact-list">
+                        <li><h2>Contact Information</h2></li>
+                        <li><a href="mailto:markrubin920@gmail.com" className="contact">Email markrubin920@gmail.com</a></li>
+                        <li><a href="linkedin.com" className="contact">LinkedIn: Mark Rubin</a></li>
+                        <li><a href="github.com" className="contact">GitHub: markrubin920</a></li>
+                    </ul>
+                </div>
+            </section>
+            <section className="footer-container">
+                {/* <BottomContainer /> */}
             </section>
         </div>
     )
