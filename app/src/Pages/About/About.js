11c23e559e03b806e0337e4b52a15457ea4663d2
markrubin920 <markrubin920@gmail.com>
2024-01-03 19:28:00 -0500

    Iphone accessability mostly working

--- app/src/Pages/About/About.js
+++ app/src/Pages/About/About.js
@@ -1,6 +1,6 @@
 import Navigation from "../../Components/Navigation/Navigation";
 import Bottom from "../../Components/Bottom Container/Bottom";
-import Headshot from "../../Media/Headshot-Square.JPG";
+import Headshot from "../../Media/Tree Pic.JPG";
 import "../../Root/Root.css";
 import "./About.css";
 
@@ -10,11 +10,49 @@ const About = () => {
       <header className="nav-background">
         <Navigation />
       </header>
-      <section className="about-me-container">
+      <div className="about-me-container">
+        <div className="text-section">
+          <h1 className="about-me-h1">About Me</h1>
+          <p className="about-me-p">
+            As a sophomore studying computer science at Princeton University, I
+            find myself immersed in a world that perfectly blends academic rigor
+            with innovation. My days are a juggling act between intense coding
+            sessions, engaging lectures, and the collaborative spirit that
+            thrives on campus. <br></br>
+            <br></br>Princeton's picturesque setting is a stark contrast to the
+            complexity of my studies. Walking through the historic campus, I am
+            constantly inspired by the blend of traditional Gothic architecture
+            and the cutting-edge research that happens within these walls. My
+            major, computer science, is not just about understanding programming
+            languages; it’s a deep dive into problem-solving and thinking
+            algorithmically. <br></br>
+            <br></br>Courses like 'Algorithms and Data Structures' challenge me
+            to think critically about efficiency and design. Here, I don’t just
+            learn how to code; I learn to innovate and apply these skills to
+            real-world problems. Collaborative projects with my peers are an
+            integral part of my learning experience, allowing us to exchange
+            diverse perspectives and ideas. <br></br>
+            <br></br>Balancing my academic life with extracurricular activities,
+            I am part of the coding club where we often participate in
+            hackathons. These events are exhilarating, pushing us to our limits
+            as we try to develop functional software or solve complex problems
+            within a limited timeframe. <br></br>
+            <br></br>As I progress through my sophomore year, I realize that
+            Princeton is not just shaping me into a skilled programmer, but a
+            thinker who can apply computer science principles to a broader
+            context. The university’s ethos of 'in the nation's service and in
+            the service of humanity' resonates deeply with me, fueling my
+            ambition to use technology for societal good.
+          </p>
+        </div>
+        <div className="image-section">
+          <img src={Headshot} alt="headshot" className="headshot-pic"></img>
+        </div>
+      </div>
+
+      {/* <section className="about-me-container">
         <div className="text">
-          <h1 className="about-me-h1">
-            <u>About Me</u>
-          </h1>
+          <h1 className="about-me-h1">About Me</h1>
           <p className="about-me-p">
             As a sophomore studying computer science at Princeton University, I
             find myself immersed in a world that perfectly blends academic rigor
@@ -50,10 +88,8 @@ const About = () => {
         <div className="picture">
           <img src={Headshot} alt="headshot" className="headshot-pic"></img>
         </div>
-      </section>
-      <footer className="footer-container">
-        <Bottom />
-      </footer>
+      </section> */}
+      <Bottom />
     </div>
   );
 };
